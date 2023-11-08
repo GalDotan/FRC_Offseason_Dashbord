082cb7eb516361d43b9d2e1340c249d2a7306d16
GalDotan <gldotan@gmail.com>
2023-11-08 21:10:05 +0200

    wip

--- src/main/java/frc/robot/subsystems/Dashboard/Dashbord.java
+++ src/main/java/frc/robot/subsystems/Dashboard/Dashbord.java
@@ -15,16 +15,30 @@ public class Dashbord extends SubsystemBase {
   /** Creates a new Dashboard. */
   private static Dashbord dashbord;
   private static MAShuffleboard dash;
-  private boolean H1;
-  private boolean H2;
-  private boolean H3;
-  private boolean M1;
-  private boolean M2;
-  private boolean M3;
-  private boolean L1;
-  private boolean L2;
-  private boolean L3;
+
+  public enum scoringPose {
+    H1,
+    H2,
+    H3,
+    M1,
+    M2,
+    M3,
+    L1,
+    L2,
+    L3
+  }
   
+  public enum intakePice {
+    Cone,
+    Cube,
+  }
+
+  public enum intakePose {
+    Floor,
+    Shelf,
+    Ramp,
+  }
+
   public Dashbord() {
     
     dash = new MAShuffleboard("commander");
@@ -33,28 +47,12 @@ public class Dashbord extends SubsystemBase {
 
   public void printGrid() {
       
-    dash.addBoolean("H1" , dash.getBoolean("H1"));
-    dash.addBoolean("H2" , dash.getBoolean("H2"));
-    dash.addBoolean("H3" , dash.getBoolean("H3"));
-    dash.addBoolean("M1" , dash.getBoolean("M1"));
-    dash.addBoolean("M2" , dash.getBoolean("M2"));
-    dash.addBoolean("M3" , dash.getBoolean("M3"));
-    dash.addBoolean("L1" , dash.getBoolean("L1"));
-    dash.addBoolean("L2" , dash.getBoolean("L2"));
-    dash.addBoolean("L3" , dash.getBoolean("L3"));
+    
 
   }
 
   public void updateGrid() { 
-    H1 = dash.getBoolean("H1");
-    H2 = dash.getBoolean("H2");
-    H3 = dash.getBoolean("H3");
-    M1 = dash.getBoolean("M1");
-    M2 = dash.getBoolean("M2");
-    M3 = dash.getBoolean("M3");
-    L1 = dash.getBoolean("L1");
-    L2 = dash.getBoolean("L2");
-    L3 = dash.getBoolean("L3");
+    
   }
 
   public static Dashbord getInstance() {
